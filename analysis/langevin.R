d31d2b08d04285bd010e016a297059fc8ad7c001
Lucas Cherkewski <lucas@lucascherkewski.com>
2020-04-02 11:04:22 -0400

    helper counter functions

--- analysis/langevin.R
+++ analysis/langevin.R
@@ -42,52 +42,65 @@ pages_mentioning_li_uids <- pages_mentioning_langevin_and_indian %>%
   
 
 ## count page occurrences by Hansard volume
-li_mention_page_count_by_doc <- page_count_by_hansard %>%
-  left_join(pages_mentioning_langevin_and_indian %>%
-              group_by(doc_id, page_section) %>%
-              summarize(mention_count = n())) %>%
-  mutate(mention_count = replace_na(mention_count, 0)) %>%
-  mutate(mention_count_prop = mention_count / page_count) %>%
-  left_join(hansard_volume_details)
+count_occurences_by_volume <- function(pages_mentioning_keyword) {
+  page_count_by_hansard %>%
+    left_join(pages_mentioning_keyword %>%
+                group_by(doc_id, page_section) %>%
+                summarize(mention_count = n())) %>%
+    mutate(mention_count = replace_na(mention_count, 0)) %>%
+    mutate(mention_count_prop = mention_count / page_count) %>%
+    left_join(hansard_volume_details)
+}
 
 ## count page occurrences by _section_ of Hansard volume
-li_mention_page_count_by_doc_section <- hansard_volume_details %>%
-  group_by(doc_id) %>%
-  pivot_longer(
-    c(frontmatter_page_count:index_page_count),
-    names_to = "section",
-    values_to = "page_count"
-  ) %>%
-  mutate(section = str_remove(section, "_page_count")) %>%
-  select(doc_id:volume, start_date, end_date, section, page_count) %>%
-  ungroup() %>%
-  left_join(
-    pages_mentioning_langevin_and_indian %>%
-      group_by(doc_id, page_section) %>%
-      summarize(mention_count = n()),
-    by = c("doc_id" = "doc_id", "section" = "page_section")
-  ) %>%
-  group_by(doc_id, section) %>%
-  mutate(mention_count = replace_na(mention_count, 0)) %>%
-  mutate(mention_count_prop = mention_count / page_count)
+count_occurences_by_section <- function(pages_mentioning_keyword) {
+  hansard_volume_details %>%
+    group_by(doc_id) %>%
+    pivot_longer(
+      c(frontmatter_page_count:index_page_count),
+      names_to = "section",
+      values_to = "page_count"
+    ) %>%
+    mutate(section = str_remove(section, "_page_count")) %>%
+    select(doc_id:volume, start_date, end_date, section, page_count) %>%
+    ungroup() %>%
+    left_join(
+      pages_mentioning_keyword %>%
+        group_by(doc_id, page_section) %>%
+        summarize(mention_count = n()),
+      by = c("doc_id" = "doc_id", "section" = "page_section")
+    ) %>%
+    group_by(doc_id, section) %>%
+    mutate(mention_count = replace_na(mention_count, 0)) %>%
+    mutate(mention_count_prop = mention_count / page_count)
+}
 
 ### regroup by parliament/session
+regroup_section_occurences_by_parlsess <- function(occurrences_by_section) {
+  occurrences_by_section %>%
+    ungroup() %>%
+    group_by(parliament, session, section) %>%
+    summarize(
+      n_vols = n_distinct(volume),
+      start_date = min(start_date),
+      end_date = max(end_date),
+      page_count = sum(page_count),
+      mention_count = sum(mention_count)
+    ) %>%
+    ungroup() %>%
+    mutate(
+      mention_count_prop = mention_count / page_count,
+      parliament_session_id = paste(parliament, session, sep = "-")
+    ) %>%
+    select(parliament_session_id, everything())
+}
+
+li_mention_page_count_by_doc <- pages_mentioning_langevin_and_indian %>%
+  count_occurences_by_volume()
+li_mention_page_count_by_doc_section <- pages_mentioning_langevin_and_indian %>%
+  count_occurences_by_section()
 li_mentions_by_parlsess_section <- li_mention_page_count_by_doc_section %>%
-  ungroup() %>%
-  group_by(parliament, session, section) %>%
-  summarize(
-    n_vols = n_distinct(volume),
-    start_date = min(start_date),
-    end_date = max(end_date),
-    page_count = sum(page_count),
-    mention_count = sum(mention_count)
-  ) %>%
-  ungroup() %>%
-  mutate(
-    mention_count_prop = mention_count / page_count,
-    parliament_session_id = paste(parliament, session, sep = "-")
-  ) %>%
-  select(parliament_session_id, everything())
+  regroup_section_occurences_by_parlsess()
   
 
 
